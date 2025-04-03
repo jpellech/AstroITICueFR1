21056af99ce2ab37f351f709be42cd8e9124f5ca
jpellech <jpellech@oberlin.edu>
2025-04-03 15:47:10 -0400

    prints available epochs

---
+++ test/print_data_keys.py
@@ -0,0 +1,21 @@
+import tdt
+
+# Load the data
+path = "(3L)ABC8L-250321-155842"
+print("finding epocs from", path)
+data = tdt.read_block(path)
+
+
+if "epocs" in data.keys():
+    print("Epochs found:", data["epocs"].keys())
+
+    """
+    for key, value in data["epocs"].items():
+        print(f"\nEpoch: {key}")
+        print("Timestamps:", value["onset"][:10])
+        print("Offsets:", value["offset"][:10])
+        # print("Values:", value["data"][:10])
+    """
+
+else:
+    print("No epoch data found.")
